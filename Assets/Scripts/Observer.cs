39b4a4979df69f4beb24f22601883ca9d9d52f19
ThompsonHVCC <56233816+ThompsonHVCC@users.noreply.github.com>
2019-11-24 17:03:24 -0500

    Observer Wired Up in Inspector
    calling an observer game object on an event by wiring up the observer in the event generators inspector.

---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public void Observation()
+    {
+        Debug.Log(transform.name + " got control in Observation method");
+    }
+}
