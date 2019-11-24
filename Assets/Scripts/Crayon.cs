6a34a5bd9ba5e1f79fc1422510e878fa958ba0eb
ThompsonHVCC <56233816+ThompsonHVCC@users.noreply.github.com>
2019-11-24 16:50:20 -0500

    Crayon Event
    Hard wired Crayon Event to flip

---
+++ Assets/Scripts/Crayon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Crayon : MonoBehaviour
+{
+    // The following estalbishes an Event, which I call crayonEvent
+    public UnityEvent crayonEvent;
+
+
+    void Update()
+    {
+        // Check to see if the user has clicked on us, and if so invoke the event handler(s) 
+        // You can think of this as "publishing" the event
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Detected left mouse button on " + transform.name);
+            crayonEvent.Invoke();
+        }
+    }
+}
